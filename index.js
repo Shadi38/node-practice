48a7160371e3ef448227c08bb87798aa301c6812
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-07-22 11:59:47 +0100

    adding 2 new file

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+
+//load the quotes JSON
+const quotes = require("./quotes.json");
+
+// Now register handlers for some routes:
+
+//   /                  - Return some helpful welcome info (text)
+app.get("/", function (request, response) {
+  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+});
+
+//   /quotes            - Should return all quotes (json)
+app.get("/quotes", (req, res) => {
+  res.send({ quotes });
+});
+//   /quotes/random     - Should return ONE quote (json)
+app.get("/quotes/random", (req, res) => {
+  res.send(pickFromArray(quotes));
+});
+
+//You can use this function to pick one element at random from a given array
+//example: pickFromArray([1,2,3,4]), or
+//example: pickFromArray(myContactsArray)
+//
+function pickFromArray(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+//Start our server so that it listens for HTTP requests!
+const listener = app.listen(process.env.PORT, function () {
+  console.log("Your app is listening on port " + listener.address().port);
+});
